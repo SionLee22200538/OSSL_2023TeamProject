f3d23d325574ee7a08037ad53f1f258e86a8d444
이시온 <ision@ision-ui-MacBookPro.local>
2023-05-20 01:18:30 +0900

    Fixed an error on main.c

--- code/Main.c
+++ code/Main.c
@@ -9,7 +9,8 @@ int main(void) {
   scanf("%d", &row);
   printf("세로 줄의 컴퓨터 수? ");
   scanf("%d", &column);
-  customer_t *s[row*column]; //최대 컴퓨터 수 만큼 빈 고객 배열 만들기
+  int total=row*column;
+  customer_t *s[total]; //최대 컴퓨터 수 만큼 빈 고객 배열 만들기
   
   int count = 0;
   count=read_file(s);  //현재 저장된 파일에서 사용중인 고객 수를 받아옴
@@ -21,31 +22,31 @@ int main(void) {
       break;
     if (menu == 1) {
       if (count != 0)
-        read_Customer(s, count);
+        read_customer(s, count);
       else 
         printf("데이터가 없습니다.\n");
     } else if (menu == 2) {
       s[count] = (customer_t*)malloc(sizeof(customer_t)); //배열에 메모리 할당
-      if (add_Customer(s[count])){
+      if (add_customer(s[count],total)){
         printf("=> 추가됨!\n");
         count++;
       }
       else free(s[count]);
     } else if (menu == 3) {
-      read_Customer(s, count);
+      read_customer(s, count);
       int num = 0;
-      printf("번호는 (취소 :0)? ");
+      printf("변경하고자 하는 고객 번호는 (취소 :0)? ");
       scanf("%d", &num);
       if (num == 0)
         break;
-      int isUpdate = add_Customer(s[num - 1]); // add_Customer()함수에서 데이터가 변경되었는지 확인 
+      int isUpdate = add_customer(s[num - 1], total); // add_customer()함수에서 데이터가 변경되었는지 확인 
       if (isUpdate == 1)
         printf("=> 수정됨!\n");
     } else if (menu == 4) {
       int num;
       int yn;
       if (count != 0) {
-        read_Customer(s, count);
+        read_customer(s, count);
         printf("삭제하실 고객의 좌석 번호는 (취소 :0)? ");
         scanf("%d", &num);
         if (num != 0) {
@@ -53,7 +54,7 @@ int main(void) {
           scanf("%d", &yn);
         }
         if (yn == 1) {
-          int isDel = delete_Customer(s, num, count);
+          int isDel = delete_customer(s, num-1, count);
           if (isDel == 1)
             printf("=> 삭제됨!\n");
           count--;
@@ -75,15 +76,19 @@ int main(void) {
     }
 
     else if (menu == 8) {
-      read_Customer(s, count);
+      read_customer(s, count);
       int num = 0;
-      printf("고객의 좌석 번호는 (취소 :0)? ");
+      printf("고객의 번호는 (취소 :0)? ");
       scanf("%d", &num);
       if (num == 0)
         break;
-      int isPay = pay(s, num, count); // add_Customer()함수에서 데이터가 변경되었는지 확인 
-      if (isPay == 1)
-        printf("=> 결제 되었습니다!\n");
+      int isPay = pay(s, num-1, count); // add_customer()함수에서 데이터가 변경되었는지 확인 
+    }
+    else if (menu == 9) {
+      current(s,count);
+    }
+    else if (menu == 10){
+      draw(s,row,column);
     }
   }
   
